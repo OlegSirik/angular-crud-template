aceb6d2151a23decc38c2ee4a696d47fc1fc6a78
OlegSirik <oleg.sirik@mail.ru>
2025-11-17 22:54:05 +0300

    Create base-api.service.ts

---
+++ src/app/shared/services/base-api.service.ts
@@ -0,0 +1,12 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Injectable } from '@angular/core';
+
+@Injectable({ providedIn: 'root' })
+export class BaseApiService {
+  constructor(private http: HttpClient) {}
+  get<T>(url: string, params?: any): Observable<T> { return this.http.get<T>(url, { params }); }
+  post<T>(url: string, body: any): Observable<T> { return this.http.post<T>(url, body); }
+  put<T>(url: string, body: any): Observable<T> { return this.http.put<T>(url, body); }
+  delete<T>(url: string): Observable<T> { return this.http.delete<T>(url); }
+}
